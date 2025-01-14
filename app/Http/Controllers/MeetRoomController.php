97befeb4ad54bd0f8b7b1c33c996096253048c1a
akiyumeyou <akiyumeyou@gmail.com>
2025-01-14 09:22:05 +0900

    コントローラ（MeetRoomMember)修正

--- app/Http/Controllers/MeetRoomController.php
+++ app/Http/Controllers/MeetRoomController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Meet;
 use App\Models\MeetRoom;
-use App\Models\MeetroomMember;
+use App\Models\MeetRoomMember;
 use App\Models\SupporterProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
